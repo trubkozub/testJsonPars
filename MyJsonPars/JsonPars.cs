2b1c897441710bc45ab8367ecbc12d94d09386d4
trubkozub <ser.trubkozub@gmail.com>
2021-05-20 16:58:21 +0300

    Add project files.

---
+++ MyJsonPars/JsonPars.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyJsonPars
+{
+    public class JsonPars
+    {
+        private static Dictionary<string, string> dictionaryJson = new Dictionary<string, string>();
+        public static Dictionary<string, string> getJsonValid(JToken token)
+        {
+            dictionaryJson.Clear();
+            return recursivJson(token);
+        }
+        private static Dictionary<string, string> recursivJson(JToken jToken)
+        {
+            foreach (JToken token in jToken.Children())
+            {
+                if (token is JProperty)
+                {
+                    var prop = token as JProperty;
+                    if (prop.Value.ToString()[0] != '{')
+                    {
+                        dictionaryJson.Add(prop.Path.Replace('.', ':'), prop.Value.ToString());
+                    }
+                }
+
+                if (token.Children().ToList().Count > 0)
+                {
+                    recursivJson(token);
+                }
+            }
+            return dictionaryJson;
+        }
+
+        public static bool Validate(string fileName, out string Error)
+        {
+            StreamReader streamReader = new StreamReader(fileName);
+
+            string json = streamReader.ReadToEnd();
+            try
+            {
+                JObject.Parse(json);
+                Error = null;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Error = $"Неверный формат JSON строки: {ex.Message}";
+                return false;
+            }
+        }
+    }
+}
---
+++ testJsonPars/Program.cs
@@ -0,0 +1,91 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testJsonPars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            do
+            {
+                string fileName = null;
+                do
+                {
+                    fileName = getFilePath();
+                }
+                while (fileName == null);
+
+                if (fileName == "exit")
+                    exit = true;
+                else
+                    printJson(fileName);
+            }
+            while (!exit);
+            Environment.Exit(0);
+        }
+
+        static void printJson(string fileName)
+        {
+            StreamReader streamReader = new StreamReader(fileName);
+
+            string json = streamReader.ReadToEnd();
+            dynamic parsedObj = JsonConvert.DeserializeObject(json);
+            var jObj = (JObject)parsedObj;
+
+            foreach (JToken token in jObj.Children())
+            {
+                foreach (KeyValuePair<string, string> keyValue in MyJsonPars.JsonPars.getJsonValid(token))
+                    Console.WriteLine(keyValue.Key + " = " + keyValue.Value);
+
+                Console.WriteLine("");
+            }
+        }
+
+        static string getFilePath()
+        {
+            string filePathReturn = null;
+            string errorFile = "It's not .json file. ";
+            string errorJson = null;
+
+            Console.Write("/Exit-enter 'c'/...Enter path to json file: ");
+            string pathFile = Console.ReadLine();
+
+            if (pathFile != "c")
+            {
+                if (isValidFile(pathFile))
+                {
+                    if (MyJsonPars.JsonPars.Validate(pathFile, out errorJson))
+                        return pathFile;
+                    else
+                        Console.Write(errorFile + errorJson);
+                }
+                else
+                    Console.Write(errorFile + errorJson);
+            }
+            else
+                filePathReturn = "exit";
+            return filePathReturn;
+        }
+
+        static bool isValidFile(string pathFile)
+        {
+            bool isValid = false;
+            string validFileType = "json";
+            string ext = Path.GetExtension(pathFile);
+
+            if (ext == "." + validFileType)
+            {
+                isValid = true;
+            }
+            return isValid;
+        }
+    }
+}
